c2a9f4319313af61b2dfdbd211b2cd90cec91c5b
Daniel Borkmann <daniel@iogearbox.net>
2021-03-05 13:59:57 +0100

    v4: support for SO_BINDTODEVICE
    In case the hostns already has a device with the service address assigned
    to it, the health checker should use SO_BINDTODEVICE to pin it to the phys
    dev instead.
    
    Example described below:
    
    On LB node:
    
      root@apoc:~# ip a
      [...]
      6: enp2s0np0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 xdp/id:1402 qdisc mq state UP [...]  <--- XDP LB dev
        link/ether 00:15:4d:13:04:dc brd ff:ff:ff:ff:ff:ff
        inet 192.168.180.29/24 scope global enp2s0np0
           valid_lft forever preferred_lft forever
        inet6 f00d::29/24 scope global
           valid_lft forever preferred_lft forever
        inet6 fe80::215:4dff:fe13:4dc/64 scope link
           valid_lft forever preferred_lft forever
      [...]
      9: cilium_ipip4@NONE: <NOARP,UP,LOWER_UP> mtu 1480 qdisc noqueue state UNKNOWN group default qlen 1000
        link/ipip 0.0.0.0 brd 0.0.0.0
      10: dummy0: <BROADCAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue state UNKNOWN group default qlen 1000
        link/ether 7e:1a:64:82:bd:61 brd ff:ff:ff:ff:ff:ff
        inet 7.7.7.7/32 scope global dummy0                      <--- SVC addr
           valid_lft forever preferred_lft forever
        inet6 fe80::7c1a:64ff:fe82:bd61/64 scope link
           valid_lft forever preferred_lft forever
    
    Without SO_BINDTODEVICE, the request does /not/ go out the enp2s0np0 dev
    where the tc egress BPF prog is sitting that will handle & push the tunnel
    case. Instead, the traffic is looped back via lo and we get a RST:
    
      root@apoc:~# tcpdump -i lo -n host 7.7.7.7 or host 192.168.180.28
      [...]
      13:56:53.628948 IP 7.7.7.7.41665 > 7.7.7.7.8080: Flags [S], seq 93515663, win 65495, options [mss 65495,sackOK,TS val 2930611131 ecr 0,nop,wscale 7], length 0
      13:56:53.628957 IP 7.7.7.7.8080 > 7.7.7.7.41665: Flags [R.], seq 0, ack 93515664, win 0, length 0
    
    With this patch, we pin the dev to enp2s0np0 via SO_BINDTODEVICE and execute
    the request again on the same env as above:
    
      root@apoc:~# ./v4 enp2s0np0 7.7.7.7 8080 192.168.180.28
    
    Now the request goes out the node and the remote host can see it:
    
      root@tank:~# tcpdump -i enp2s0np0 -n
      [...]
      14:07:06.767306 IP 192.168.180.29 > 192.168.180.28: IP 192.168.180.29.35697 > 7.7.7.7.8080: Flags [S], seq 225468299, win 64240, options [mss 1460,sackOK,TS val 3042231612 ecr 0,nop,wscale 7], length 0 (ipip-proto-4)
    
    For testing the old behavior, we also support the "any" keyword where the
    SO_BINDTODEVICE is not performed:
    
      root@apoc:~# ./v4 any 7.7.7.7 8080 192.168.180.28
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- v4.c
+++ v4.c
@@ -1,6 +1,6 @@
 #if 0
  # gcc -Wall -O2 v4.c -o v4
- # ./v4 1.1.1.1 8080 10.10.10.1 10.10.10.2 10.10.10.3
+ # ./v4 any|<dev> 1.1.1.1 8080 10.10.10.1 10.10.10.2 10.10.10.3
 #endif
 
 #include <stdio.h>
@@ -23,29 +23,29 @@ int main(int argc, char **argv)
 	struct sockaddr_in f_addr = {
 		.sin_family	= AF_INET,
 	}, b_addr[128] = {};
-	const int b_max = sizeof(b_addr) / sizeof(b_addr[0]), b_real = argc - 3;
+	const int b_max = sizeof(b_addr) / sizeof(b_addr[0]), b_real = argc - 4;
 
-	if (argc < 4 || b_real > b_max) {
-		fprintf(stderr, "Usage: %s [fe] [port] [be1] [be2] ... [beN] (N_max=%d)\n",
+	if (argc < 5 || b_real > b_max) {
+		fprintf(stderr, "Usage: %s any|<dev> [fe] [port] [be1] [be2] ... [beN] (N_max=%d)\n",
 			argv[0], b_max);
 		return -1;
 	}
 
-	ret = inet_pton(AF_INET, argv[1], &f_addr.sin_addr);
+	ret = inet_pton(AF_INET, argv[2], &f_addr.sin_addr);
 	if (ret != 1) {
-		fprintf(stderr, "pton(%s): %s\n", argv[1],
+		fprintf(stderr, "pton(%s): %s\n", argv[2],
 			strerror(errno));
 		return ret;
 	}
 
-	f_addr.sin_port = htons(atoi(argv[2]));
+	f_addr.sin_port = htons(atoi(argv[3]));
 	for (i = 0; i < b_real; i++) {
 		b_addr[i].sin_family = f_addr.sin_family;
 		b_addr[i].sin_port   = f_addr.sin_port;
 
-		ret = inet_pton(AF_INET, argv[3 + i], &b_addr[i].sin_addr);
+		ret = inet_pton(AF_INET, argv[4 + i], &b_addr[i].sin_addr);
 		if (ret != 1) {
-			fprintf(stderr, "pton(%s): %s\n", argv[3 + i],
+			fprintf(stderr, "pton(%s): %s\n", argv[4 + i],
 				strerror(errno));
 			return ret;
 		}
@@ -55,6 +55,17 @@ int main(int argc, char **argv)
 	if (fd < 0) {
 		perror("socket");
 		return -1;
+	}
+
+	if (strcmp(argv[1], "any")) {
+		ret = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
+				 argv[1], strlen(argv[1]));
+		if (ret < 0) {
+			perror("setsockopt(SO_BINDTODEVICE)");
+			goto out;
+		}
+
+		printf("Bound health checker to device: %s\n", argv[1]);
 	}
 
 	ret = setsockopt(fd, SOL_SOCKET, SO_MARK, &mark, sizeof(mark));
@@ -78,14 +89,14 @@ int main(int argc, char **argv)
 	for (i = 0; i < b_real; i++) {
 		ret = bind(fd, (struct sockaddr*)&b_addr[i], sizeof(b_addr[i]));
 		if (ret < 0) {
-			fprintf(stderr, "bind(%s): %s\n", argv[3 + i],
+			fprintf(stderr, "bind(%s): %s\n", argv[4 + i],
 				strerror(errno));
 			goto out;
 		}
 
 		ret = connect(fd, (struct sockaddr*)&f_addr, sizeof(f_addr));
 		if (ret < 0) {
-			fprintf(stderr, "backend down(%s): %s\n", argv[3 + i],
+			fprintf(stderr, "backend down(%s): %s\n", argv[4 + i],
 				strerror(errno));
 			cnt++;
 		}
@@ -94,7 +105,7 @@ int main(int argc, char **argv)
 	}
 	ret = 0;
 	printf("Summary for %s:%d: %d/%d backends reachable\n",
-	       argv[1], ntohs(f_addr.sin_port), b_real - cnt, b_real);
+	       argv[2], ntohs(f_addr.sin_port), b_real - cnt, b_real);
 out:
 	close(fd);
 	return ret;
